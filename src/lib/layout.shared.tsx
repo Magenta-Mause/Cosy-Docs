8bf619f8ce62ca021c6a138a780a8ac295266b94
Janne Keipert <jabbekeipert@gmail.com>
2026-03-18 14:05:45 +0100

    change discord banner behaivior

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,17 +1,9 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
-import { DISCORD_INVITE_URL } from "./constants";
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
       title: "COSY Docs",
     },
-    links: [
-      {
-        text: "Discord",
-        url: DISCORD_INVITE_URL,
-        external: true,
-      },
-    ],
   };
 }
